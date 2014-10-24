a91fb3a48aa6dff05a91a6ba1444542d3296852d
PICHLEAP SOK <psok@luc.edu>
2014-10-23 23:14:39 -0500

    First import

---
+++ carousel/jcarousel.basic.js
@@ -0,0 +1,72 @@
+(function($) {
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
+(function($, Modernizr) {
+    $(function() {
+        $('.jcarousel')
+            .jcarousel({
+                wrap: 'circular'
+            })
+            .jcarouselAutoscroll({
+                interval: 6000,
+                target: '+=1',
+                autostart: true
+            });
+        $('.jcarousel').jcarousel({
+            'transitions': true
+        });
+        $('.jcarousel').jcarousel({
+            wrap: 'circular',
+            transitions: Modernizr.csstransitions ? {
+                transforms:   Modernizr.csstransforms,
+                transforms3d: Modernizr.csstransforms3d,
+                easing: 'ease'
+            } : false
+        });
+
+        $('.jcarousel-control-prev').jcarouselControl({
+            target: '-=1'
+        });
+
+        $('.jcarousel-control-next').jcarouselControl({
+            target: '+=1'
+        });
+
+        $('.jcarousel').jcarousel({
+    animation: 'slow'
+});
+});
+})(jQuery, Modernizr);
